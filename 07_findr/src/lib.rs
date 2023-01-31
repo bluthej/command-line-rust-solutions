09105813aa0e7585178c33cbda85c06e6ff5d578
bluthej <joffrey.bluthe@e.email>
2023-01-31 19:43:51 +0100

    Start work on 07_findr
    - Set up argument parsing
    - Copy `tests` directory from official repo

---
+++ 07_findr/src/lib.rs
@@ -0,0 +1,33 @@
+use clap::{Parser, ValueEnum};
+use std::error::Error;
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    #[arg(help = "Search paths", default_value = ".")]
+    path: Vec<String>,
+
+    #[arg(short, long, help = "Name", num_args(0..))]
+    name: Vec<String>,
+
+    #[arg(short = 't', long = "type", help = "Entry type", value_name = "TYPE", num_args(0..))]
+    entry_type: Vec<EntryType>,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum EntryType {
+    F,
+    D,
+    L,
+}
+
+pub fn get_args() -> MyResult<Cli> {
+    Cli::try_parse().map_err(|e| e.into())
+}
+
+pub fn run(cli: Cli) -> MyResult<()> {
+    println!("{:?}", cli);
+    Ok(())
+}
