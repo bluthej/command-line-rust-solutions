fdd7ddae17e844ed2fa1eeab2f8799ff129a5647
bluthej <joffrey.bluthe@e.email>
2023-03-04 12:10:30 +0100

    Add `find_lines` function + tests

--- 09_grepr/src/lib.rs
+++ 09_grepr/src/lib.rs
@@ -86,8 +86,11 @@ fn find_files(paths: &[String], recursive: bool) -> Vec<MyResult<String>> {
 
 #[cfg(test)]
 mod tests {
-    use super::find_files;
+    use crate::{find_files, find_lines};
     use rand::{distributions::Alphanumeric, Rng};
+    use regex::{Regex, RegexBuilder};
+    use std::io::Cursor;
+
     #[test]
     fn test_find_files() {
         // Verify that the function finds a file known to exist
@@ -128,6 +131,55 @@ mod tests {
         assert_eq!(files.len(), 1);
         assert!(files[0].is_err());
     }
+
+    #[test]
+    fn test_find_lines() {
+        let text = b"Lorem\nIpsum\r\nDOLOR";
+        // The pattern _or_ should match the one line, "Lorem"
+        let re1 = Regex::new("or").unwrap();
+        let matches = find_lines(Cursor::new(&text), &re1, false);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 1);
+        // When inverted, the function should match the other two lines
+        let matches = find_lines(Cursor::new(&text), &re1, true);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 2);
+        // This regex will be case-insensitive
+        let re2 = RegexBuilder::new("or")
+            .case_insensitive(true)
+            .build()
+            .unwrap();
+        // The two lines "Lorem" and "DOLOR" should match
+        let matches = find_lines(Cursor::new(&text), &re2, false);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 2);
+        // When inverted, the one remaining line should match
+        let matches = find_lines(Cursor::new(&text), &re2, true);
+        assert!(matches.is_ok());
+        assert_eq!(matches.unwrap().len(), 1);
+    }
+}
+
+fn find_lines<T: BufRead>(
+    mut file: T,
+    pattern: &Regex,
+    invert_match: bool,
+) -> MyResult<Vec<String>> {
+    file.lines()
+        .collect::<Result<Vec<String>, _>>()
+        .map(|lines| {
+            lines
+                .into_iter()
+                .filter(|line| {
+                    if invert_match {
+                        !pattern.is_match(line)
+                    } else {
+                        pattern.is_match(line)
+                    }
+                })
+                .collect()
+        })
+        .map_err(|e| format!("{}", e).into())
 }
 
 fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
