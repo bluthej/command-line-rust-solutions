4868545a2fa9a12d64ce909d3867bdec30381673
bluthej <joffrey.bluthe@e.email>
2023-04-04 23:39:08 +0200

    Implement `run`
    Pass all the tests

--- 10_commr/src/lib.rs
+++ 10_commr/src/lib.rs
@@ -1,4 +1,5 @@
 use std::{
+    cmp::Ordering,
     error::Error,
     fs::File,
     io::{self, BufRead, BufReader},
@@ -56,9 +57,62 @@ pub fn run(cli: Cli) -> MyResult<()> {
         return Err("Both input files cannot be STDIN (\"-\")".into());
     }
 
-    let _file1 = open(file1)?;
-    let _file2 = open(file2)?;
-    println!("Opened {} and {}", file1, file2);
+    let del = cli.delimiter.to_string();
+    let pref2 = del.repeat(cli.show_col1 as usize);
+    let pref3 = del.repeat(cli.show_col1 as usize + cli.show_col2 as usize);
+
+    let choose_case = |line: String| {
+        if cli.insensitive {
+            line.to_lowercase()
+        } else {
+            line
+        }
+    };
+
+    let file1 = open(file1)?;
+    let file2 = open(file2)?;
+    let mut lines1 = file1.lines().filter_map(|l| l.ok().map(choose_case));
+    let mut lines2 = file2.lines().filter_map(|l| l.ok().map(choose_case));
+    let mut line1 = lines1.next();
+    let mut line2 = lines2.next();
+    loop {
+        match (&line1, &line2) {
+            (Some(l1), Some(l2)) => match l1.cmp(l2) {
+                Ordering::Less => {
+                    if cli.show_col1 {
+                        println!("{}", l1);
+                    }
+                    line1 = lines1.next();
+                }
+                Ordering::Greater => {
+                    if cli.show_col2 {
+                        println!("{}{}", pref2, l2);
+                    }
+                    line2 = lines2.next();
+                }
+                Ordering::Equal => {
+                    if cli.show_col3 {
+                        println!("{}{}", pref3, l1);
+                    }
+                    line1 = lines1.next();
+                    line2 = lines2.next();
+                }
+            },
+            (Some(l1), None) => {
+                if cli.show_col1 {
+                    println!("{}", l1);
+                }
+                line1 = lines1.next();
+            }
+            (None, Some(l2)) => {
+                if cli.show_col2 {
+                    println!("{}{}", pref2, l2);
+                }
+                line2 = lines2.next();
+            }
+            (None, None) => break,
+        }
+    }
 
     Ok(())
 }
