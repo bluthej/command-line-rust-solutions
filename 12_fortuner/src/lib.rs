54129b701837e3a08c4ab935d8f5e675a17cefe3
bluthej <joffrey.bluthe@e.email>
2023-04-21 18:36:27 +0200

    Implement `find_files` function

--- 12_fortuner/src/lib.rs
+++ 12_fortuner/src/lib.rs
@@ -1,5 +1,6 @@
 use regex::{Regex, RegexBuilder};
-use std::error::Error;
+use std::{error::Error, path::PathBuf};
+use walkdir::WalkDir;
 
 use clap::Parser;
 
@@ -39,4 +40,65 @@ pub fn get_args() -> MyResult<Cli> {
 pub fn run(cli: Cli) -> MyResult<()> {
     println!("{:#?}", cli);
     Ok(())
+}
+
+fn find_files(paths: &[String]) -> MyResult<Vec<PathBuf>> {
+    let mut res = Vec::new();
+    for path in paths {
+        for entry in WalkDir::new(path) {
+            let path = entry?.into_path();
+            if path.is_file() && !path.ends_with(".dat") {
+                res.push(path);
+            }
+        }
+    }
+    res.sort();
+    res.dedup();
+    Ok(res)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::find_files;
+
+    #[test]
+    fn test_find_files() {
+        // Verify that the function finds a file known to exist
+        let res = find_files(&["./tests/inputs/jokes".to_string()]);
+        assert!(res.is_ok());
+        let files = res.unwrap();
+        assert_eq!(files.len(), 1);
+        assert_eq!(
+            files.get(0).unwrap().to_string_lossy(),
+            "./tests/inputs/jokes"
+        );
+        // Fails to find a bad file
+        let res = find_files(&["/path/does/not/exist".to_string()]);
+        assert!(res.is_err());
+        // Finds all the input files, excludes ".dat"
+        let res = find_files(&["./tests/inputs".to_string()]);
+        assert!(res.is_ok());
+        // Check number and order of files
+        let files = res.unwrap();
+        assert_eq!(files.len(), 5);
+        let first = files.get(0).unwrap().display().to_string();
+        assert!(first.contains("ascii-art"));
+        let last = files.last().unwrap().display().to_string();
+        assert!(last.contains("quotes"));
+        // Test for multiple sources, path must be unique and sorted
+        let res = find_files(&[
+            "./tests/inputs/jokes".to_string(),
+            "./tests/inputs/ascii-art".to_string(),
+            "./tests/inputs/jokes".to_string(),
+        ]);
+        assert!(res.is_ok());
+        let files = res.unwrap();
+        assert_eq!(files.len(), 2);
+        if let Some(filename) = files.first().unwrap().file_name() {
+            assert_eq!(filename.to_string_lossy(), "ascii-art".to_string())
+        }
+        if let Some(filename) = files.last().unwrap().file_name() {
+            assert_eq!(filename.to_string_lossy(), "jokes".to_string())
+        }
+    }
 }
