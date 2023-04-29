44578955c484ff129544c33cc3b87d9195422438
bluthej <joffrey.bluthe@e.email>
2023-04-29 11:07:40 +0200

    Create `Month` type and finish argument parsing

--- 13_calr/src/lib.rs
+++ 13_calr/src/lib.rs
@@ -1,30 +1,28 @@
+mod month;
+
 use std::error::Error;
 
+use clap::error::Result;
 use clap::Parser;
+
+use crate::month::Month;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 pub struct Cli {
-    #[arg(short, help = "Month name or number (1-12)")]
-    month: Option<u32>,
+    /// Month name or number (1-12)
+    #[arg(short)]
+    month: Option<Month>,
 
-    #[arg(short = 'y', long = "year", help = "Show whole current year")]
+    /// Show whole current year
+    #[arg(short = 'y', long = "year")]
     show_year: bool,
 
-    #[arg(value_name = "YEAR", help = "Year (1-9999)", value_parser = check_year_range)]
-    year: u32,
-}
-
-fn check_year_range(s: &str) -> Result<u32, String> {
-    s.parse().map_err(|e| format!("{e}")).and_then(|year| {
-        if !(1..=9999).contains(&year) {
-            Err("the accepted range is 1-9999".to_string())
-        } else {
-            Ok(year)
-        }
-    })
+    /// Year (1-9999)
+    #[arg(value_name = "YEAR", value_parser = clap::value_parser!(u32).range(..=9999))]
+    year: Option<u32>,
 }
 
 pub fn get_args() -> MyResult<Cli> {
