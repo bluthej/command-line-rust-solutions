6fc9420895c3327fbb0e283347a5f69f3b612195
bluthej <joffrey.bluthe@e.email>
2023-04-30 14:40:59 +0200

    Implement argument parser

--- 14_lsr/src/lib.rs
+++ 14_lsr/src/lib.rs
@@ -6,13 +6,25 @@ type MyResult<T> = Result<T, Box<dyn Error>>;
 
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
-pub struct Cli {}
+pub struct Cli {
+    /// Files and/or directories
+    #[arg(default_value = ".", value_name = "PATH")]
+    paths: Vec<String>,
+
+    /// Long listing
+    #[arg(short, long)]
+    long: bool,
+
+    /// Show all files
+    #[arg(short, long)]
+    all: bool,
+}
 
 pub fn get_args() -> MyResult<Cli> {
     Cli::try_parse().map_err(From::from)
 }
 
 pub fn run(cli: Cli) -> MyResult<()> {
-    println!("{:?}", cli);
+    println!("{:#?}", cli);
     Ok(())
 }
