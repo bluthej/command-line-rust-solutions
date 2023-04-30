9af06a490bf63cbf6c3d74c0b655c590fded1aef
bluthej <joffrey.bluthe@e.email>
2023-04-30 19:19:06 +0200

    Implement `find_files` function

--- 14_lsr/src/lib.rs
+++ 14_lsr/src/lib.rs
@@ -1,6 +1,9 @@
 use clap::error::Result;
 use clap::Parser;
 use std::error::Error;
+use std::fs;
+use std::path::PathBuf;
+use std::str::FromStr;
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -27,4 +30,127 @@ pub fn get_args() -> MyResult<Cli> {
 pub fn run(cli: Cli) -> MyResult<()> {
     println!("{:#?}", cli);
     Ok(())
+}
+
+fn find_files(paths: &[String], show_hidden: bool) -> MyResult<Vec<PathBuf>> {
+    let mut res = Vec::new();
+    for path in paths.iter().map(PathBuf::from) {
+        if path.is_file() {
+            res.push(path);
+        } else if path.is_dir() {
+            let Ok(dir) = path.read_dir() else {
+                eprintln!("{}: no such file or directory", path.display());
+                continue;
+            };
+            for item in dir {
+                let Ok(entry) = item else {
+                    eprintln!("{}: no such file or directory", path.display());
+                    continue;
+                };
+                let path = entry.path();
+                if show_hidden || !entry.file_name().to_string_lossy().starts_with('.') {
+                    res.push(path);
+                }
+            }
+        } else {
+            unimplemented!("I don't know what to do with symlinks");
+        }
+    }
+    Ok(res)
+}
+
+#[cfg(test)]
+mod test {
+    use super::find_files;
+
+    #[test]
+    fn test_find_files() {
+        // Find all nonhidden entries in a directory
+        let res = find_files(&["tests/inputs".to_string()], false);
+        assert!(res.is_ok());
+        let mut filenames: Vec<_> = res
+            .unwrap()
+            .iter()
+            .map(|entry| entry.display().to_string())
+            .collect();
+        filenames.sort();
+        assert_eq!(
+            filenames,
+            [
+                "tests/inputs/bustle.txt",
+                "tests/inputs/dir",
+                "tests/inputs/empty.txt",
+                "tests/inputs/fox.txt",
+            ]
+        );
+        // Find all entries in a directory
+        let res = find_files(&["tests/inputs".to_string()], true);
+        assert!(res.is_ok());
+        let mut filenames: Vec<_> = res
+            .unwrap()
+            .iter()
+            .map(|entry| entry.display().to_string())
+            .collect();
+        filenames.sort();
+        assert_eq!(
+            filenames,
+            [
+                "tests/inputs/.hidden",
+                "tests/inputs/bustle.txt",
+                "tests/inputs/dir",
+                "tests/inputs/empty.txt",
+                "tests/inputs/fox.txt",
+            ]
+        );
+        // Any existing file should be found even if hidden
+        let res = find_files(&["tests/inputs/.hidden".to_string()], false);
+        assert!(res.is_ok());
+        let filenames: Vec<_> = res
+            .unwrap()
+            .iter()
+            .map(|entry| entry.display().to_string())
+            .collect();
+        assert_eq!(filenames, ["tests/inputs/.hidden"]);
+        // Test multiple path arguments
+        let res = find_files(
+            &[
+                "tests/inputs/bustle.txt".to_string(),
+                "tests/inputs/dir".to_string(),
+            ],
+            false,
+        );
+        assert!(res.is_ok());
+        let mut filenames: Vec<_> = res
+            .unwrap()
+            .iter()
+            .map(|entry| entry.display().to_string())
+            .collect();
+        filenames.sort();
+        assert_eq!(
+            filenames,
+            ["tests/inputs/bustle.txt", "tests/inputs/dir/spiders.txt"]
+        );
+    }
+
+    #[test]
+    fn test_find_files_hidden() {
+        let res = find_files(&["tests/inputs".to_string()], true);
+        assert!(res.is_ok());
+        let mut filenames: Vec<_> = res
+            .unwrap()
+            .iter()
+            .map(|entry| entry.display().to_string())
+            .collect();
+        filenames.sort();
+        assert_eq!(
+            filenames,
+            [
+                "tests/inputs/.hidden",
+                "tests/inputs/bustle.txt",
+                "tests/inputs/dir",
+                "tests/inputs/empty.txt",
+                "tests/inputs/fox.txt",
+            ]
+        );
+    }
 }
