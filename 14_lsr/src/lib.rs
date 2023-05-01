d9c9934a62eafa198a88ee00cfefaed7219f8586
bluthej <joffrey.bluthe@e.email>
2023-05-01 10:55:38 +0200

    Implement `format_output` function
    Remains to format permissions

--- 14_lsr/src/lib.rs
+++ 14_lsr/src/lib.rs
@@ -1,9 +1,10 @@
+use chrono::{DateTime, Local};
 use clap::error::Result;
 use clap::Parser;
-use std::error::Error;
-use std::fs;
 use std::path::PathBuf;
-use std::str::FromStr;
+use std::{error::Error, os::unix::prelude::MetadataExt};
+use tabular::{Row, Table};
+use users::{get_group_by_gid, get_user_by_uid};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -29,9 +30,8 @@ pub fn get_args() -> MyResult<Cli> {
 
 pub fn run(cli: Cli) -> MyResult<()> {
     let paths = find_files(&cli.paths, cli.all)?;
-    for path in paths {
-        println!("{}", path.display());
-    }
+    let s = format_output(&paths)?;
+    println!("{}", s);
     Ok(())
 }
 
@@ -60,6 +60,34 @@ fn find_files(paths: &[String], show_hidden: bool) -> MyResult<Vec<PathBuf>> {
         }
     }
     Ok(res)
+}
+
+fn format_output(paths: &[PathBuf]) -> MyResult<String> {
+    let fmt = "{:<}{:<} {:>} {:<} {:<} {:>} {:<} {:<}";
+    let mut table = Table::new(fmt);
+    for path in paths {
+        let file_type = if path.is_dir() { "d" } else { "-" };
+        let metadata = path.metadata()?;
+        let permissions = metadata.mode();
+        let n_links = metadata.nlink();
+        let user = get_user_by_uid(metadata.uid()).unwrap();
+        let group = get_group_by_gid(metadata.gid()).unwrap();
+        let size = metadata.len();
+        let modif: DateTime<Local> = metadata.modified()?.into();
+        let modif = modif.format("%b %e %y %R");
+        table.add_row(
+            Row::new()
+                .with_cell(file_type)
+                .with_cell(permissions)
+                .with_cell(n_links)
+                .with_cell(user.name().to_string_lossy())
+                .with_cell(group.name().to_string_lossy())
+                .with_cell(size)
+                .with_cell(modif)
+                .with_cell(path.display()),
+        );
+    }
+    Ok(format!("{}", table))
 }
 
 #[cfg(test)]
